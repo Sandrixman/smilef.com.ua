83229bfa3ddad8d455426ed40ca953e8f1d77426
Aleksander <sandrix@ukr.net>
2022-01-24 16:51:54 +0200

    24.01.22

--- components/priceTable.php
+++ components/priceTable.php
@@ -2,164 +2,290 @@
 <tbody><tr class="main">
 <td>КАТЕГОРІЯ НОМЕРА</td>
 <td>ВИД</td>
+<td>ХАРЧУВАННЯ</td>
 <td>ОСНОВНИХ МІСЦЬ</td>
-<td>МАКСИМУМ МІСЦЬ</td>
-<td>ЦІНА 01.01 - 17.06</td>
-<td>ЦІНА 18.06 - 01.09</td>
-<td>ЦІНА 02.09 - 01.01</td>
+<td>ДОДАТКОВИХ МІСЦЬ</td>
+<td>ЦІНА 01.01 - 22.04.2022</td>
+<td>ЦІНА 23.04 - 09.05.2022</td>
+<td>ЦІНА 10.05 - 02.06.2022</td>
+<td>ЦІНА 03.06 - 09.06.2022</td>
+<td>ЦІНА 10.06 - 31.08.2022</td>
+<td>ЦІНА 01.09 - 31.12.2022</td>
 </tr>
 <tr class="studio-g1">
 <td><a href="/studio-g1.html">Studio G1</a></td>
-<td>Вид у двір, 1 поверх</td>
+<td>Вид на басейн і парк, 1 поверх</td>
+<td>з кухнею</td>
 <td>2</td>
-<td>3</td>
-<td class="price-studio-g1">1800</td>
-<td>2400</td>
+<td>1</td>
+<td>1600</td>
 <td>2000</td>
+<td class="price-studio-g1">1600</td>
+<td>1600</td>
+<td>1600</td>
+<td>1600</td>
 </tr>
 <tr class="studio-g1-1">
-<td><a href="/studio-g1-1.html">Studio G1-1</a></td>
-<td>Вид на басейн і парк, 1 этаж</td>
+<td><a href="/studio-g1-1.html">Studio G1\1</a></td>
+<td>Вид у двір, 1 поверх</td>
+<td>з їжою на 2 чол</td>
 <td>2</td>
-<td>3</td>
-<td class="price-studio-g1-1">2200</td>
-<td>2800</td>
-<td>2400</td>
+<td>1</td>
+<td>x</td>
+<td>2600</td>
+<td class="price-studio-g1-1">2400</td>
+<td>2600</td>
+<td>3200</td>
+<td>2600</td>
 </tr>
 <tr class="studio-g2">
 <td><a href="/studio-g2.html">Studio G2</a></td>
 <td>Вид на море, степ, парк, басейн, 2 поверх</td>
+<td>з їжою на 2 чол</td>
 <td>2</td>
-<td>3</td>
-<td class="price-studio-g2">2200</td>
-<td>2800</td>
-<td>2400</td>
+<td>1</td>
+<td>x</td>
+<td>2600</td>
+<td class="price-studio-g2">2400</td>
+<td>2600</td>
+<td>3300</td>
+<td>2600</td>
 </tr>
 <tr class="delux-g2">
-<td><a href="/delux-g2.html">delux G2</a></td>
+<td><a href="/delux-g2.html">Delux G2</a></td>
 <td>Вид на море, 2 поверх</td>
+<td>з їжою на 2 чол</td>
 <td>2</td>
-<td>3</td>
-<td class="price-delux-g2">2400</td>
-<td>3200</td>
-<td>2600</td>
+<td>2</td>
+<td>x</td>
+<td>2800</td>
+<td class="price-delux-g2">2500</td>
+<td>2800</td>
+<td>3500</td>
+<td>2800</td>
 </tr>
 <tr class="superior-g2">
-<td><a href="/superior-g2.html">superior G2</a></td>
+<td><a href="/superior-g2.html">Superior G2</a></td>
 <td>Вид на лиман, ресторан, 2 поверх</td>
+<td>з їжою на 2 чол</td>
 <td>2</td>
-<td>2</td>
-<td class="price-superior-g2">1600</td>
+<td>1</td>
+<td>x</td>
+<td>1900</td>
+<td class="price-superior-g2">1700</td>
+<td>1900</td>
 <td>2400</td>
-<td>1800</td>
+<td>1900</td>
 </tr>
 <tr class="apartment-sea-1">
-<td><a href="/apartment-sea-1.html">APARTMENT SEA 1</a></td>
+<td><a href="/apartment-sea-1.html">Apartment Sea1 (1,3,4)
+</a></td>
 <td>Вид на море, алею, 1 поверх</td>
+<td>з кухнею</td>
 <td>3</td>
-<td>4</td>
-<td class="price-apartment-sea-1">2500</td>
-<td>3300</td>
-<td>2700</td>
+<td>1</td>
+<td>2200</td>
+<td>3000</td>
+<td class="price-apartment-sea-1">2200</td>
+<td>3000</td>
+<td>4200</td>
+<td>3000</td>
+</tr>
+<tr class="apartment-sea-1f">
+<td><a href="/apartment-sea-1f.html">Apartment Sea1\F
+</a></td>
+<td>Вид на море, алею, 1 поверх</td>
+<td>з їжою на 2 чол</td>
+<td>3</td>
+<td>1</td>
+<td>2200</td>
+<td>3000</td>
+<td class="price-apartment-sea-1f">2200</td>
+<td>3000</td>
+<td>4200</td>
+<td>3000</td>
 </tr>
 <tr class="apartment-sea-2">
-<td><a href="/apartment-sea-2.html">APARTMENT SEA 2</a></td>
+<td><a href="/apartment-sea-2.html">Apartment Sea2 (1,3)
+</a></td>
 <td>Вид на море, алею, 1 поверх</td>
+<td>з кухнею</td>
 <td>4</td>
-<td>5</td>
-<td class="price-apartment-sea-2">3600</td>
-<td>4800</td>
-<td>3800</td>
+<td>1</td>
+<td>3200</td>
+<td>4000</td>
+<td class="price-apartment-sea-2">3200</td>
+<td>4000</td>
+<td>5600</td>
+<td>4000</td>
+</tr>
+<tr class="apartment-sea-2a">
+<td><a href="/apartment-sea-2a.html">Apartment Sea2 (4)
+</a></td>
+<td>Вид на море, алею, 1 поверх</td>
+<td>з кухнею</td>
+<td>4</td>
+<td>1</td>
+<td>3200</td>
+<td>3700</td>
+<td class="price-apartment-sea-2a">3200</td>
+<td>3700</td>
+<td>5200</td>
+<td>3700</td>
+</tr>
+<tr class="apartment-sea-2f">
+<td><a href="/apartment-sea-2f.html">Apartment Sea2\F
+</a></td>
+<td>Вид на море, алею, 1 поверх</td>
+<td>з їжою на 2 чол</td>
+<td>4</td>
+<td>1</td>
+<td>x</td>
+<td>4500</td>
+<td class="price-apartment-sea-2f">4100</td>
+<td>4500</td>
+<td>5600</td>
+<td>4500</td>
 </tr>
 <tr class="apartment-sea-3">
-<td><a href="/apartment-sea-3.html">APARTMENT SEA 3</a></td>
+<td><a href="/apartment-sea-3.html">Apartment Sea3
+</a></td>
 <td>Вид на море, алею, 1 поверх</td>
+<td>з кухнею</td>
 <td>6</td>
-<td>7</td>
-<td class="price-apartment-sea-3">4500</td>
-<td>6000</td>
-<td>4700</td>
+<td>1</td>
+<td>4000</td>
+<td>5000</td>
+<td class="price-apartment-sea-3">4000</td>
+<td>5000</td>
+<td>7000</td>
+<td>5000</td>
 </tr>
 <tr class="standart">
-<td><a href="/standart.html">standart</a></td>
+<td><a href="/standart.html">Standart
+</a></td>
 <td>Вид на парк, алею, 1 поверх</td>
+<td>з їжою на 2 чол</td>
 <td>1</td>
 <td>1</td>
-<td class="price-standart">1400</td>
-<td>1900</td>
-<td>1600</td>
+<td>x</td>
+<td>1800</td>
+<td class="price-standart">1600</td>
+<td>1800</td>
+<td>2200</td>
+<td>1800</td>
 </tr>
 <tr class="apartment-park">
-<td><a href="/apartment-park.html">APARTMENT PARK</a></td>
+<td><a href="/apartment-park.html">Apartment Park
+</a></td>
 <td>Вид на парк, 1 поверх</td>
+<td>з їжою на 2 чол</td>
 <td>4</td>
-<td>5</td>
-<td class="price-apartment-park">4400</td>
-<td>5200</td>
-<td>4600</td>
-</tr>
-<tr class="villa-west">
-<td><a href="/villa-west.html">VILLA ЗАХІД</a></td>
-<td>Вид на море, 1 поверх</td>
-<td>4</td>
-<td>5</td>
-<td class="price-villa-west">2100</td>
-<td>4200</td>
-<td>2400</td>
-</tr>
-<tr class="villa-south">
-<td><a href="/villa-south.html">VILLA південь</a></td>
-<td>Вид на море, 1 поверх</td>
-<td>4</td>
-<td>6</td>
-<td class="price-villa-south">2500</td>
-<td>4500</td>
-<td>2600</td>
+<td>1</td>
+<td>x</td>
+<td>4300</td>
+<td class="price-apartment-park">3900</td>
+<td>4300</td>
+<td>5400</td>
+<td>4300</td>
 </tr>
 <tr class="villa-east">
-<td><a href="/villa-east.html">VILLA схід</a></td>
+<td><a href="/villa-east.html">Villa Cxiд
+</a></td>
 <td>Вид на море, 1 поверх</td>
+<td>з кухнею</td>
 <td>4</td>
-<td>5</td>
-<td class="price-villa-east">2100</td>
-<td>4200</td>
-<td>2400</td>
+<td>1</td>
+<td>1900</td>
+<td>3400</td>
+<td class="price-villa-east">1900</td>
+<td>3400</td>
+<td>4700</td>
+<td>3400</td>
+</tr>
+<tr class="villa-west">
+<td><a href="/villa-west.html">Villa Захiд
+</a></td>
+<td>Вид на море, 1 поверх</td>
+<td>з кухнею</td>
+<td>4</td>
+<td>1</td>
+<td>1900</td>
+<td>3400</td>
+<td class="price-villa-west">1900</td>
+<td>3400</td>
+<td>4700</td>
+<td>3400</td>
+</tr>
+<tr class="villa-south">
+<td><a href="/villa-south.html">Villa Пiвдень
+</a></td>
+<td>Вид на море, 1 поверх</td>
+<td>з кухнею</td>
+<td>4</td>
+<td>2</td>
+<td>2200</td>
+<td>3600</td>
+<td class="price-villa-south">2200</td>
+<td>3600</td>
+<td>5000</td>
+<td>3600</td>
 </tr>
 <tr class="villa-vip">
-<td><a href="/villa-vip.html">VILLA vip</a></td>
+<td><a href="/villa-vip.html">Villa Vip
+</a></td>
 <td>Вид на море, двухповерховий</td>
+<td>з кухнею</td>
 <td>6</td>
-<td>7</td>
-<td class="price-villa-vip">3200</td>
-<td>5500</td>
-<td>3200</td>
+<td>1</td>
+<td>2900</td>
+<td>4300</td>
+<td class="price-villa-vip">2900</td>
+<td>4300</td>
+<td>6000</td>
+<td>4300</td>
 </tr>
 <tr class="studio-north">
-<td><a href="/studio-north.html">STUDIO ПІВНІЧ</a></td>
-<td>Вид у двір, 1 этаж</td>
+<td><a href="/studio-north.html">Studio пiвнiч
+</a></td>
+<td>Вид у двір, 1 поверх</td>
+<td>з кухнею</td>
 <td>2</td>
-<td>2</td>
-<td class="price-studio-north">1500</td>
-<td>1900</td>
-<td>1500</td>
-</tr>
-<tr class="cottage-sky">
-<td><a href="/cottage-sky.html">COTTAGE НЕБЕСНИЙ</a></td>
-<td>Вид на море і двір, 2 поверх</td>
-<td>4</td>
-<td>5</td>
-<td class="price-cottage-sky">1600</td>
-<td>3000</td>
-<td>1700</td>
+<td>1</td>
+<td>1350</td>
+<td>1400</td>
+<td class="price-studio-north">1350</td>
+<td>1400</td>
+<td>2000</td>
+<td>1400</td>
 </tr>
 <tr class="studio-sky">
-<td><a href="/studio-sky.html">STUDIO ПІДНЕБЕСНА</a></td>
+<td><a href="/studio-sky.html">Studio пiднебесний
+</a></td>
 <td>Вид у двір, 1 поверх</td>
+<td>з кухнею</td>
 <td>2</td>
-<td>2</td>
-<td class="price-studio-sky">1500</td>
-<td>1900</td>
-<td>1500</td>
+<td>1</td>
+<td>1350</td>
+<td>1600</td>
+<td class="price-studio-sky">1350</td>
+<td>1600</td>
+<td>2200</td>
+<td>1600</td>
+</tr>
+<tr class="cottage-sky">
+<td><a href="/cottage-sky.html">Соttaqe небесний
+</a></td>
+<td>Вид на море і двір, 2 поверх</td>
+<td>з кухнею</td>
+<td>4</td>
+<td>1</td>
+<td>1450</td>
+<td>2400</td>
+<td class="price-cottage-sky">1450</td>
+<td>2400</td>
+<td>3300</td>
+<td>2400</td>
 </tr>
 </tbody></table>
 </body></html>
